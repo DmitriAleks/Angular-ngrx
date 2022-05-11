b7f941641c77a543412d6ee7720546274371432f
Dmitri Aleks <alexeyonok.dmitriy@neatsoft.io>
2022-05-11 18:11:09 +0300

    ngrx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-Ngrx';
+  counter = 0;
+  updateNumber? : number
+
+
+ 
+ get cannotDecrease():boolean {
+   return this.counter <= 0
+ }
+
+  increse():void {
+    this.updateNumber= Date.now()
+   this.counter++
+ }
+decrease():void {
+  this.updateNumber= Date.now()
+  this.counter--
+}
+
+clear():void {
+  this.updateNumber= Date.now()
+  this.counter = 0
+}
+
 }
